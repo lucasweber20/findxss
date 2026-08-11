3ac077c393ed6ade96eee6023b43c7e991e7df3a
lucasweber20 <lucasweber449@gmail.com>
2026-08-11 14:02:56 -0300

    Feat: add arguments flags

--- findxss.py
+++ findxss.py
@@ -1,7 +1,20 @@
+import argparse
 
+
+parser = argparse.ArgumentParser()
+
+args = parser.add_argument("-u", "--url", help='Specify url, example: -u https://example.com/?param=value', nargs="+", type=str)
+args = parser.add_argument("-l", "--list", help="Specify file with urls, example: -l urls.txt", type=str)
+args = parser.add_argument("-t", "--thread", help="Specify threads number, example: -t 2", default=1, type=int)
+args = parser.add_argument("-o", "--output", help="Specify output file, example: -o outputs.txt", type=str)
+
+args = parser.parse_args()
 
 def main():
-    pass
+    url = args.url
+    file = args.file
+    thread = args.thred
+    output = args.output
 
 if __name__ == "__main__":
     main()
